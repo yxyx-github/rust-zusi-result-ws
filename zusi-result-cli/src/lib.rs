3543a78fe24e76599ebd0847518c45ee7eb4d2d4
yxyx-github <dev_mw@vodafonemail.de>
2025-03-26 12:56:50 +0100

    refactor error conversion of cli

--- zusi-result-cli/src/lib.rs
+++ zusi-result-cli/src/lib.rs
@@ -18,12 +18,24 @@ pub enum AnalyseFilesError {
     PrintAnalysisError(PrintAnalysisError),
 }
 
+impl From<PatternError> for AnalyseFilesError {
+    fn from(value: PatternError) -> Self {
+        AnalyseFilesError::PatternError(value)
+    }
+}
+
+impl From<PrintAnalysisError> for AnalyseFilesError {
+    fn from(value: PrintAnalysisError) -> Self {
+        AnalyseFilesError::PrintAnalysisError(value)
+    }
+}
+
 pub fn analyse_files(args: AnalyseFilesArgs) -> Result<(), AnalyseFilesError> {
     println!("Analyse files by pattern: {}", args.pattern);
 
     let mut results: Vec<ZusiResult> = vec![];
 
-    for entry in glob(&args.pattern).map_err(|e| AnalyseFilesError::PatternError(e))? {
+    for entry in glob(&args.pattern)? {
         match entry {
             Ok(path) => {
                 match read_result(&path) {
@@ -33,26 +45,16 @@ pub fn analyse_files(args: AnalyseFilesArgs) -> Result<(), AnalyseFilesError> {
                         }
                         results.push(result);
                     }
-                    Err(ReadResultError::IOError(e)) => {
-                        eprintln!("Error reading file '{:?}': {:?}", path, e);
-                    }
-                    Err(ReadResultError::DeError(e)) => {
-                        eprintln!("Error during deserialization of '{:?}': {:?}", path, e);
-                    }
-                    Err(ReadResultError::NoResult) => {
-                        eprintln!("The file '{:?}' does not contain a result.", path);
-                    }
+                    Err(e) => eprintln!("{}", e.message(path)),
                 }
             }
-            Err(e) => {
-                eprintln!("{:?}", e);
-            }
+            Err(e) => eprintln!("{:?}", e),
         }
     }
 
     println!();
     println!("Analysis results:");
-    print_analysis(results).map_err(|e| AnalyseFilesError::PrintAnalysisError(e))?;
+    print_analysis(results)?;
     Ok(())
 }
 
@@ -63,11 +65,39 @@ enum ReadResultError {
     NoResult,
 }
 
+impl From<io::Error> for ReadResultError {
+    fn from(value: io::Error) -> Self {
+        ReadResultError::IOError(value)
+    }
+}
+
+impl From<DeError> for ReadResultError {
+    fn from(value: DeError) -> Self {
+        ReadResultError::DeError(value)
+    }
+}
+
+impl ReadResultError {
+    fn message(&self, path: PathBuf) -> String {
+        match self {
+            ReadResultError::IOError(e) => {
+                format!("Error reading file '{:?}': {:?}", path, e)
+            }
+            ReadResultError::DeError(e) => {
+                format!("Error during deserialization of '{:?}': {:?}", path, e)
+            }
+            ReadResultError::NoResult => {
+                format!("The file '{:?}' does not contain a result.", path)
+            }
+        }
+    }
+}
+
 fn read_result(path: &PathBuf) -> Result<ZusiResult, ReadResultError> {
-    let mut input_file = File::open(path).map_err(|err| ReadResultError::IOError(err))?;
+    let mut input_file = File::open(path)?;
     let mut contents = String::new();
-    input_file.read_to_string(&mut contents).map_err(|err| ReadResultError::IOError(err))?;
-    let zusi = Zusi::from_xml(&contents).map_err(|err| ReadResultError::DeError(err))?;
+    input_file.read_to_string(&mut contents)?;
+    let zusi = Zusi::from_xml(&contents)?;
     for value in zusi.value {
         if let ZusiValue::Result(result) = value {
             return Ok(result);
@@ -82,22 +112,34 @@ pub enum PrintAnalysisError {
     AnalyseError(AnalyseError),
 }
 
+impl From<AnalyseError> for PrintAnalysisError {
+    fn from(value: AnalyseError) -> Self {
+        PrintAnalysisError::AnalyseError(value)
+    }
+}
+
+impl From<CreateAnalyserGroupError> for PrintAnalysisError {
+    fn from(value: CreateAnalyserGroupError) -> Self {
+        PrintAnalysisError::CreateAnalyserGroupError(value)
+    }
+}
+
 fn print_analysis(results: Vec<ZusiResult>) -> Result<(), PrintAnalysisError> {
-    let mut analyser_group: ResultAnalyserGroup<_, _> = results.try_into().map_err(|e| PrintAnalysisError::CreateAnalyserGroupError(e))?;
+    let mut analyser_group: ResultAnalyserGroup<_, _> = results.try_into()?;
 
-    println!("total distance: {} m", analyser_group.total_distance().map_err(|e| PrintAnalysisError::AnalyseError(e))?);
+    println!("total distance: {} m", analyser_group.total_distance()?);
 
-    println!("average distance: {} m", analyser_group.average_distance().map_err(|e| PrintAnalysisError::AnalyseError(e))?);
+    println!("average distance: {} m", analyser_group.average_distance()?);
 
-    let average_speed = analyser_group.average_speed().map_err(|e| PrintAnalysisError::AnalyseError(e))?;
+    let average_speed = analyser_group.average_speed()?;
     println!("average speed: {} m/s = {} km/h", average_speed, average_speed * 3.6);
 
-    let pure_average_speed = analyser_group.pure_average_speed(PureAverageSpeedAlgorithm::default()).map_err(|e| PrintAnalysisError::AnalyseError(e))?;
+    let pure_average_speed = analyser_group.pure_average_speed(PureAverageSpeedAlgorithm::default())?;
     println!("pure average speed: {} m/s = {} km/h", pure_average_speed, pure_average_speed * 3.6);
 
-    println!("total driving time: {}", analyser_group.total_driving_time().map_err(|e| PrintAnalysisError::AnalyseError(e))?);
+    println!("total driving time: {}", analyser_group.total_driving_time()?);
 
-    println!("total pure driving time: {}", analyser_group.total_pure_driving_time().map_err(|e| PrintAnalysisError::AnalyseError(e))?);
+    println!("total pure driving time: {}", analyser_group.total_pure_driving_time()?);
 
     Ok(())
 }
